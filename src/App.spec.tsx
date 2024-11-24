778207cf17579c0f9343a3100c18b5a15995a7e2
Chris Freeman <cwfreeman@gmail.com>
2024-11-24 13:56:07 -0600

    Handling input changes.

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,23 @@
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
 import App from "./App.tsx";
 
 describe('The app', () => {
-    it('should have inputs and a button for scheimpflug calculations', () => {
+    it('should have inputs and a button for scheimpflug calculations', async () => {
         render(<App />);
-        screen.getByRole('textbox', { name: 'Focal Plane'});
-        screen.getByRole('textbox', { name: 'Focal Length'});
-        screen.getByRole('button', { name: 'Calculate' });
+        const focalPlaneInput = screen.getByRole('textbox', { name: 'Focal Plane'});
+        const focalLengthInput = screen.getByRole('textbox', { name: 'Focal Length'});
+        const angleOfFocusOutput = screen.getByRole('status', { name: 'Angle of Focus'});
+
+        expect(angleOfFocusOutput.innerText).toBe('');
+
+        await userEvent.type(focalPlaneInput, '12')
+        expect(angleOfFocusOutput.innerText).toBe('');
+
+        console.log(`Going to type into focal length input`)
+        await userEvent.type(focalLengthInput, '34');
+
+        expect(angleOfFocusOutput.innerText).not.toBe('');
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import './App.css'
+import React, {useState} from "react";
+
 
 function App() {
+    const [focalPlaneValue, setFocalPlaneValue] = useState(0);
+    const [focalLengthValue, setFocalLengthValue] = useState(0);
+    const angleOfFocus = calculateAngleOfFocus();
+
+    function calculateAngleOfFocus() {
+        if (focalLengthValue === 0 || focalPlaneValue === 0) return ''
+        return '90';
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<number>>) {
+        setter(Number.parseInt(event.target.value));
+    }
+
+    function handleFocalPlaneChange(event: React.ChangeEvent<HTMLInputElement>) {
+        console.log(`Changing focal plane to ${event.target.value}`)
+        handleChange(event, setFocalPlaneValue);
+    }
+    function handleFocalLengthChange(event: React.ChangeEvent<HTMLInputElement>) {
+        console.log(`Changing focal length to ${event.target.value}`)
+        handleChange(event, setFocalLengthValue);
+    }
+
+
     return (
     <>
       <h1>Scheimpflug Calculator</h1>
       <div className="card">
           <label htmlFor="focal_plane">Focal Plane</label>
-          <input type="text" id="focal_plane" />
+          <input type="text" id="focal_plane" value={focalPlaneValue} onChange={handleFocalPlaneChange}/>
           <label htmlFor="focal_length">Focal Length</label>
-          <input type="text" id="focal_length" />
-        <button>Calculate</button>
+          <input type="text" id="focal_length" value={focalLengthValue} onChange={handleFocalLengthChange}/>
+          <div role='status' aria-label='Angle of Focus'>{angleOfFocus}</div>
       </div>
     </>
   )
